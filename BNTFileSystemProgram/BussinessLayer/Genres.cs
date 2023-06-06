721b8b5bf3242f0fb1c675b78a3f053d9140aa62
Rev0ld <ivan_mladenov2004@abv.bg>
2023-06-06 13:27:34 +0300

    Added User and Roles
    Started Genres

---
+++ BNTFileSystemProgram/BussinessLayer/Genres.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Mime;
+
+namespace BussinessLayer
+{
+    public class Genres
+    {
+        [Key]
+        public string Id { get; private set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        //TODO: add list of entries in videos
+
+    }
+}
---
+++ BNTFileSystemProgram/BussinessLayer/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace BussinessLayer
+{
+    public class User : IdentityUser
+    {
+        [Required, DisplayName("First name")]
+        public string FirstName { get; set; }
+
+        [Required, DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        public string Biography { get; set; }
+
+        [Required]
+        public Role Role { get; set; }
+    }
+}
